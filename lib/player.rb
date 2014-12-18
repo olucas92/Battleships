ef06370140a536126bc26ef6622057b9597989ca
guspowell <augustuspowell@me.com>
2014-12-18 17:54:46 +0000

    changed to match ollies commit

--- lib/player.rb
+++ lib/player.rb
@@ -29,13 +29,8 @@ class Player
   end
 
   def adjoining(coords)
-<<<<<<< HEAD
-    previous_coord = coords.shift
-    coords.map do |item|
-=======
     previous_coord = coords.dup.shift
     coords.each do |item|
->>>>>>> ef6b89605d81ec02b51c471a373fdad84d7608a5
       return true if (previous_coord.next == item) || previous_coord.reverse.next.reverse == item 
     end 
     false
